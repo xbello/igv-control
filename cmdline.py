5a275b2d5fd9a787218ea9a0166be0231c415676
Xabi Bello <xbello@gmail.com>
2015-12-29 13:09:33 +0100

    Refactored socket into context manager.

--- cmdline.py
+++ cmdline.py
@@ -2,39 +2,54 @@
 import socket
 
 
+class SocketManager():
+
+    """A Context Manager to deal with Sockets."""
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+    def __enter__(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.host, self.port))
+        self.socket.settimeout(1)
+
+        return self.socket
+
+    def __exit__(self, *exc_info):
+        self.socket.close()
+
+
 class IGV():
 
     """IGV wrapper to control the program through a socket."""
+    def __init__(self, host="", port=60151):
+        self.host = host
+        self.port = port
 
     def check_igv(self):
         """Return True if a copy of IGV is reachable."""
 
-        self.open_socket()
         response = ""
-        self.socket.sendall(bytes("echo", "ascii"))
-        response = str(self.socket.recv(1024), "ascii")
-
-        self.close()
+        with SocketManager(self.host, self.port) as s:
+            s.sendall(bytes("echo", "ascii"))
+            response = str(s.recv(1024), "ascii")
 
         if response == "echo":
             return True
         return False
-
-    def close(self):
-        """Close the socket if it is open."""
-        self.socket.close()
 
     def command(self, command):
         pass
 
     def goto(self, position):
         """Return "True" if IGV answered "OK" to a goto command."""
-        response = ""
 
-        self.open_socket()
-        self.socket.sendall(bytes("goto {}".format(position), "ascii"))
-        response = str(self.socket.recv(1024), "ascii")
-        self.close()
+        response = ""
+        with SocketManager(self.host, self.port) as s:
+            s.sendall(bytes("goto {}".format(position), "ascii"))
+            response = str(s.recv(1024), "ascii")
 
         if response.startswith("OK"):
             return True
@@ -42,18 +57,12 @@ class IGV():
 
     def load(self, filepath):
         """Return "True" if IGV answered "OK" to a load command."""
-        response = ""
 
-        self.open_socket()
-        self.socket.sendall(bytes("load {}".format(filepath), "ascii"))
-        response = str(self.socket.recv(1024), "ascii")
-        self.close()
+        response = ""
+        with SocketManager(self.host, self.port) as s:
+            s.sendall(bytes("load {}".format(filepath), "ascii"))
+            response = str(s.recv(1024), "ascii")
 
         if response.startswith("OK"):
             return True
         return False
-
-    def open_socket(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect(("", 60151))
-        self.socket.settimeout(1)
