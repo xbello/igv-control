a299006202b8e3de653c6050c85886380531d7a7
Xabi Bello <xbello@gmail.com>
2016-01-08 13:35:39 +0100

    Refactored cmdline into helpers.
    Created cmline as true cmdline enter point.

--- cmdline.py
+++ cmdline.py
@@ -1,147 +1,47 @@
 """Provide access through command line to IGV controlling."""
-import socket
-
-import vcf
+import helpers
 
 
-class SocketManager():
-
-    """A Context Manager to deal with Sockets."""
-
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.socket = None
-
-    def __enter__(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((self.host, self.port))
-        self.socket.settimeout(1)
-
-        return self.socket
-
-    def __exit__(self, *exc_info):
-        self.socket.close()
+def main(cmd_args):
+    """Launch the controller either through tkinter or command line."""
+    if cmd_args.gui:
+        print("Here we launch the GUI.")
+    elif cmd_args.variants:
+        variants = helpers.Variants(cmd_args.variants)
+        # Here we launch the command line with args.variants.")
+    else:
+        # No GUI and no FilePath provided.
+        variant_file = input("Enter the file path (or quit with [q]): ")
+        if variant_file != "q":
+            print("Here we launch the command line with variant_file.")
 
 
-class IGV():
+if __name__ == "__main__":
+    import argparse
 
-    """IGV wrapper to control the program through a socket."""
+    parser = argparse.ArgumentParser(description="Manage IGV through sockets.")
+    parser.add_argument("--variants",
+                        help="Path to file with variants. It works with VCF " +
+                        "or TAB files.")
+    parser.add_argument("--gui", type=bool,
+                        help="Launch a Tkinter Gui to control IGV")
+    args = parser.parse_args()
 
-    def __init__(self, host="", port=60151):
-        self.host = host
-        self.port = port
+    main(args)
 
-    def check_igv(self):
-        """Return True if a copy of IGV is reachable."""
+    import readchar
 
-        response = self.command("echo")
+    print("Press <- or ->, q to quit")
 
-        if response.startswith("echo"):
-            return True
-        return False
+    while True:
+        c = readchar.readkey()
+        LEFT = readchar.key.LEFT
+        RIGHT = readchar.key.RIGHT
 
-    def command(self, command):
-        """Return the response from IGV for command."""
-        response = ""
-        with SocketManager(self.host, self.port) as s:
-            s.sendall(bytes(command, "ascii"))
-            response = str(s.recv(1024), "ascii")
-
-        return response
-
-    def goto(self, position):
-        """Return "True" if IGV answered "OK" to a goto command."""
-
-        response = self.command("goto {}".format(position))
-
-        if response.startswith("OK"):
-            return True
-        return False
-
-    def load(self, filepath):
-        """Return "True" if IGV answered "OK" to a load command."""
-
-        response = self.command("load {}".format(filepath))
-
-        if response.startswith("OK"):
-            return True
-        return False
-
-
-class Variants():
-
-    """A proxy to the variants contained in a VCF or TAB file.
-
-    Initialize with a file name::
-
-        >>> variants = Variants("path/to/file.vcf")
-        >>>
-
-    Get the variants through a generator at __next__::
-
-        >>> for variant in variants:
-        ...     print(variant)
-        ("chr1", "123456")
-        ("chr1", "456789")
-
-    """
-
-    def __init__(self, filename):
-        self.filename = filename
-        self.variants = None
-        self.load()
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        return next(self.variants)
-
-    def load(self):
-        """Set a generator from a filetab if it's a VCF or a TAB file."""
-        if not self.loadvcf():
-            self.loadtab()
-
-    def loadtab(self):
-        """Return a generator if the filepath is a valid VCF 4.0 file."""
-        with open(self.filename) as tabfile:
-            first_line = tabfile.readline().split("\t")
-            if len(first_line) < 5:
-                # This doesn't seem to be a valid tab file.
-                return False
-
-        self.variants = self.tab_generator()
-        return True
-
-    def loadvcf(self):
-        """Return a generator if the filepath is a valid VCF 4.0 file."""
-        vcf_reader = vcf.Reader(open(self.filename), prepend_chr=True)
-
-        if vcf_reader.infos:
-            # It seems to be a valid VCF file
-            self.variants = self.vcf_generator(vcf_reader)
-            return True
-
-        return False
-
-    def tab_generator(self):
-        """Yield line by line from a tab file except the header line."""
-        first_line = False
-        with open(self.filename) as tabfile:
-            for line in tabfile:
-                s_line = tuple(line.split("\t")[:2])
-                if first_line:
-                    yield s_line
-                else:
-                    first_line = line.lower().split("\t")
-                    if not any([_ in first_line for _ in
-                                ["start", "end", "alt", "ref"]]):
-                        # This file doesn't have a header
-                        yield s_line
-
-    @staticmethod
-    def vcf_generator(generator):
-        """Yield only the fields we are interested in from the VCF."""
-        for variant in generator:
-            yield (variant.CHROM, str(variant.POS))
+        if c in ("q", "Q",
+                 readchar.key.CTRL_C,
+                 readchar.key.CTRL_D,
+                 readchar.key.CTRL_Z):
+            break
+        if c in (LEFT, RIGHT):
+            print("Seems OK")
--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -1,107 +1,21 @@
 """Tests for the command line interface."""
 import os
-import socketserver
-import threading
-from unittest import TestCase
+from unittest import mock, TestCase
 
 import cmdline
 
 
-class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
-    def handle(self):
-        """Return the same it receives."""
-        data = str(self.request.recv(1024), 'ascii')
+class TestMain(TestCase):
+    @mock.patch("helpers.Variants")
+    def test_main_launches_commandline(self, variants_mock):
+        args = mock.Mock()
+        args.gui = False
+        sample_path = os.path.join(os.path.dirname(__file__),
+                                   "files", "example.tab")
+        args.variants = sample_path
 
-        response = bytes("", "ascii")
-        if data == "echo":
-            response = bytes("{}".format(data), 'ascii')
-        elif data.startswith(("goto", "load")):
-            response = bytes("OK\n", 'ascii')
+        cmdline.main(args)
 
-        self.request.sendall(response)
+        variants_mock.assert_called_with(sample_path)
 
-
-class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-    allow_reuse_address = True
-
-
-class TestCmd(TestCase):
-    def setUp(self):
-        super().setUp()
-        self.igv_server = ThreadedTCPServer(
-            ("", 60151), ThreadedTCPRequestHandler)
-
-        server_thread = threading.Thread(target=self.igv_server.serve_forever)
-        server_thread.daemon = True
-        server_thread.start()
-
-        self.igv_client = cmdline.IGV()
-
-    def tearDown(self):
-        self.igv_server.shutdown()
-        self.igv_server.server_close()
-        super().tearDown()
-
-    def test_can_IGV_client_as_object(self):
-        self.assertTrue(isinstance(self.igv_client, cmdline.IGV))
-
-    def test_we_can_send_multiple_commands(self):
-        self.assertTrue(self.igv_client.check_igv())
-        self.assertTrue(self.igv_client.goto("chr1:12345"))
-
-    def test_check_igv_is_running(self):
-        self.assertTrue(self.igv_client.check_igv())
-
-    def test_we_can_send_goto_signal(self):
-        self.assertTrue(self.igv_client.goto("chr1:123456"))
-
-    def test_we_can_send_load_signal(self):
-        self.assertTrue(self.igv_client.load("path/to/file.bam"))
-
-    def test_we_can_send_through_command_method(self):
-        self.assertEqual(self.igv_client.command("echo"), "echo")
-
-
-class TestVCFandTAB(TestCase):
-    def setUp(self):
-        super().setUp()
-        self.vcf_file = os.path.join(os.path.dirname(__file__),
-                                     "files/example-4.0.vcf")
-        self.tab_file = os.path.join(os.path.dirname(__file__),
-                                     "files/example.tab")
-        self.tab_file_noheader = os.path.join(os.path.dirname(__file__),
-                                              "files/example_noheader.tab")
-
-        self.variants_vcf = cmdline.Variants(self.vcf_file)
-        self.variants_tab = cmdline.Variants(self.tab_file)
-        self.variants_tab_noh = cmdline.Variants(self.tab_file_noheader)
-
-    def test_can_load_vcf_file(self):
-        self.assertTrue(self.variants_vcf.loadvcf())
-        self.assertFalse(self.variants_tab.loadvcf())
-
-        self.assertEqual(len([_ for _ in self.variants_vcf]), 5)
-
-    def test_can_load_tab_file(self):
-        self.assertFalse(self.variants_vcf.loadtab())
-        self.assertTrue(self.variants_tab.loadtab())
-
-        self.assertEqual(len([_ for _ in self.variants_tab]), 3)
-
-    def test_tab_generator(self):
-        self.assertEqual(
-            len([_ for _ in self.variants_tab.tab_generator()]), 3)
-        self.assertEqual(
-            len([_ for _ in self.variants_tab_noh.tab_generator()]), 3)
-
-    def test_generate_chrompos_pairs_from_vcf_and_tab(self):
-        self.assertEqual([("chr20", "14370"),
-                          ("chr20", "17330"),
-                          ("chr20", "1110696"),
-                          ("chr20", "1230237"),
-                          ("chr20", "1234567")],
-                         [_ for _ in self.variants_vcf])
-        self.assertEqual([("chr1", "7571115"),
-                          ("chr1", "7572645"),
-                          ("chr1", "7573472")],
-                         [_ for _ in self.variants_tab])
+        self.fail("Continue testing here")
